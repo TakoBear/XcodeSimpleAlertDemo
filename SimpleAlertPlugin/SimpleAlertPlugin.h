3fe8e5d66fac3924849b8784950033bb7c1c6ffc
shouian <amark9@hotmail.com>
2014-03-01 22:15:04 +0800

    Initial Commit

---
+++ SimpleAlertPlugin/SimpleAlertPlugin.h
@@ -0,0 +1,13 @@
+//
+//  SimpleAlertPlugin.h
+//  SimpleAlertPlugin
+//
+//  Created by shouian on 2014/3/1.
+//  Copyright (c) 2014年 shouian. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface SimpleAlertPlugin : NSObject
+
+@end
---
+++ SimpleAlertPlugin/SimpleAlertPlugin.m
@@ -0,0 +1,64 @@
+//
+//  SimpleAlertPlugin.m
+//  SimpleAlertPlugin
+//
+//  Created by shouian on 2014/3/1.
+//    Copyright (c) 2014年 shouian. All rights reserved.
+//
+
+#import "SimpleAlertPlugin.h"
+
+static SimpleAlertPlugin *sharedPlugin;
+
+@interface SimpleAlertPlugin()
+
+@property (nonatomic, strong) NSBundle *bundle;
+@end
+
+@implementation SimpleAlertPlugin
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+    if ([currentApplicationName isEqual:@"Xcode"]) {
+        dispatch_once(&onceToken, ^{
+            sharedPlugin = [[self alloc] initWithBundle:plugin];
+        });
+    }
+}
+
+- (id)initWithBundle:(NSBundle *)plugin
+{
+    if (self = [super init]) {
+        // reference to plugin's bundle, for resource acccess
+        self.bundle = plugin;
+        
+        // Create menu items, initialize UI, etc.
+
+        // Sample Menu Item:
+        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
+        if (menuItem) {
+            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+            [actionMenuItem setTarget:self];
+            [[menuItem submenu] addItem:actionMenuItem];
+        }
+    }
+    return self;
+}
+
+// Sample Action, for menu item:
+- (void)doMenuAction
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
